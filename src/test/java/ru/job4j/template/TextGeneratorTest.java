175a0aa3c743cadc393cb43d8d3ec9f2b3b173ad
Traindriver-M <sergioparovozov@mail.ru>
2022-02-08 23:24:19 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/TextGeneratorTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+import ru.job4j.tdd.Ticket3D;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TextGeneratorTest {
+
+    @Test
+    public void whenStringGenerated() {
+        TextGenerator textGenerator = new TextGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name","subject");
+        String expected = String.format("I am a %s, Who are %s? ", "name", map.get("name"));
+        assertThat(expected, is(textGenerator.produce(template,map)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoKey() {
+        TextGenerator textGenerator = new TextGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        textGenerator.produce(template,map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExtraKey() {
+        TextGenerator textGenerator = new TextGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("surname","subject");
+        textGenerator.produce(template,map);
+    }
+}
