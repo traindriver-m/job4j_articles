b2ec46e48b1ca982ed1b8207e1e9e5a3577a6d46
Traindriver-M <sergioparovozov@mail.ru>
2022-02-10 01:30:01 +0300

    3. Шаблонизатор. [#855 #262240]

--- src/test/java/ru/job4j/template/TextGeneratorTest.java
+++ src/test/java/ru/job4j/template/TextGeneratorTest.java
@@ -11,33 +11,37 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class TextGeneratorTest {
-    @Ignore
+
     @Test
     public void whenStringGenerated() {
         TextGenerator textGenerator = new TextGenerator();
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = new HashMap<>();
-        map.put("name", "subject");
-        String expected = String.format("I am a %s, Who are %s? ", "name", map.get("name"));
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        String expected = String.format("I am a %s, Who are %s? ",  map.get("name"), map.get("subject"));
         assertThat(expected, is(textGenerator.produce(template, map)));
     }
 
-    @Ignore
+
     @Test(expected = IllegalArgumentException.class)
     public void whenNoKey() {
         TextGenerator textGenerator = new TextGenerator();
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
         textGenerator.produce(template, map);
     }
 
-    @Ignore
+
     @Test(expected = IllegalArgumentException.class)
     public void whenExtraKey() {
         TextGenerator textGenerator = new TextGenerator();
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = new HashMap<>();
-        map.put("surname", "subject");
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        map.put("age", "50");
         textGenerator.produce(template, map);
     }
 }
